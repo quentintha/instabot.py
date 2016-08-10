1dea138a13f69a7f5ec8739ca544cadebaf8fd75
quentintha <quinticasals@gmail.com>
2016-08-10 22:51:19 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,14 +3,14 @@
 
 from instabot import InstaBot
 
-bot = InstaBot(login="my_username", password="my_password",
+bot = InstaBot(login='el_rajola', password='casals94',
                like_per_day=1000,
-               comments_per_day=0,
-               tag_list=['follow4follow', 'f4f', 'cute'],
+               comments_per_day=3,
+               tag_list=['follow4follow', 'f4f', 'cute', 'portrait', 'art', 'artist', 'instaartist', 'painting', 'oil', 'picoftheday', 'oilpainting', 'artoftheday', 'igers', 'beautiful'],
                max_like_for_one_tag=50,
                follow_per_day=150,
-               follow_time=5*60*60,
-               unfollow_per_day=150,
+               follow_time=24*60*60,
+               unfollow_per_day=145,
                unfollow_break_min=15,
                unfollow_break_max=30,
                log_mod=0)
